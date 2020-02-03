c042db9069c83a4e73c3482dff24a1fa516344f4
Zahra Khan <57815553+ZahraKhan1812@users.noreply.github.com>
2020-02-03 12:36:11 -0500

    Add files via upload

---
+++ PracticeWeek2.java
@@ -0,0 +1,31 @@
+ /**
+  *  @author Zahra
+  *  created : 2.3.20
+  *  description: Variable Practice
+  *  
+  **/
+public class PracticeWeek2 {
+	 public static void main(String[] args) {
+   //Defining Variables
+		 double wallet = 10.35;
+		 byte purse    = 5;
+		 int digit     = 3;
+	
+		//Declaring Variables
+	    String name1;
+	    String name2;
+         String name3;
+    
+    //Assigning Values
+    name1 = "Kyle";
+    name2 ="Nora";
+    name3 = "Joy";
+   
+    //Printing
+    System.out.println(name1);
+	System.out.println(name1+name3);//Expected: KyleJoy
+
+	
+	}
+	
+}
