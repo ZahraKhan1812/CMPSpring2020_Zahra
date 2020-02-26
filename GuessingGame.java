ebfae3b03f7fcf5990b79976994a22e6ff121308
Zahra Khan <57815553+ZahraKhan1812@users.noreply.github.com>
2020-02-26 12:27:07 -0500

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,20 +1,27 @@
-import java.swing.JOptionpane;
+/**
+ * 
+ * @author Zahra
+ *
+ */
 import java.util.Random;
-public class RandomNumberGuessingGame{
-  public static void main(String [] args) {
-    
-    Random random = new Random();
-    int randomNumber = random.nextInt( 45 );
-    boolean userCorrect = false;
-    String userInputString;
-    int userGuessedNumber;
-    
-    while( !userCorrect ) {
-      userInputString = JOptionpane.showInputDialog("Guess the number; ");
-      userGuessedNumber = Integer.parseInt(userInputString);
-      if(userGuessedNumber > randomNumber) {
-        JOptionpane.showInputDialog
-      }
-    }
-  }
+import java.util.Scanner;
+
+public class NumberGuesser {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		Random rand = new Random();
+		System.out.println("Welcome to the number guesser");
+		
+		int randPick = rand.nextInt(3);
+		System.out.println("Enter a number from 0 to 5: ");
+		int userPick = kb.nextInt();
+		
+		if(userPick==randPick)
+			for(int i=0;i<3;i++)
+				System.out.println("vicotry");
+		else
+			for(int i=0;i<3;i++)
+				System.out.println("Game over");
+				
+	}
 }
